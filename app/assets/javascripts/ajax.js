5916174e1fcce84163155763a3afccf12c6c82fa
wildraj <rajalspatel@gmail.com>
2015-09-19 13:51:57 -0700

    Ajaxed JSON to backend

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-  console.log('rajal is the punmaster');
+  //When outside, Handlebars was not defined
+  // Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
+  //     lvalue = parseFloat(lvalue);
+  //     rvalue = parseFloat(rvalue);
+
+  //     return {
+  //         '+': lvalue + rvalue,
+  //         '-': lvalue - rvalue,
+  //         '*': lvalue * rvalue,
+  //         '/': lvalue / rvalue,
+  //         '%': lvalue % rvalue
+  //     }[operator];
+  // });
   $('#github-search').on('submit', function(event){
     event.preventDefault();
     configure = {
@@ -9,11 +21,23 @@ $(document).ready(function() {
     }
     $.ajax(configure)
     .done(function(response){
-      var source = $('#search-results').html();
-      var template = Handlebars.compile(source);
-      var html = template(response["items"]);
+      console.log(response['items']);
+      var configure = {
+        url: "/plans/createplan",
+        method: 'post',
+        data: {repos: response['items']},
+        dataType: 'json'
+      }
+      $.ajax(configure)
+      .done(function(response){
+
+      });
+      //var source = $('#search-results').html();
+      //var template = Handlebars.compile(source);
+      //var html = template(response["items"]);
       //debugger
-      $('#github-search-stuff').html(html);
+      //$('#github-search-stuff').html(response['items']);
     });
   });
 });
+
