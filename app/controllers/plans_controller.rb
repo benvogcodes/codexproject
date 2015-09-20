7535c75cabad635ab8657bd9620c6eb2f607ce1e
Michael Ashby <ashby.michael@gmail.com>
2015-09-19 19:08:52 -0700

    create repos (cards) from github api data

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -13,6 +13,19 @@ class PlansController < ApplicationController
   end
 
   def testcreate
+    @user = User.find(session[:user_id])
+    puts '*************************'
+    puts @user.id
+    puts '*************************'
+    @data = params
+    new_plan = @user.plans.new(frequency: 'na', topic: 'na', query: 'na')
+    new_plan.save
+    puts '*************************'
+    puts "plan: #{new_plan.id}"
+    puts '*************************'
+    @data = new_plan.create_plan(@data, @user)
+
+    render json: @data
   end
 
   def show
