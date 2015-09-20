8898d27c605bea45b42aa2ac9a2e55b3abb39dac
wildraj <rajalspatel@gmail.com>
2015-09-20 12:00:19 -0700

    Adjust Ajax to send params to backend

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -16,15 +16,7 @@ $(document).ready(function() {
     event.preventDefault();
     var data = $(this).serialize();
     // Make query into OOJS
-    for (var i = 0; i < data.length; i++){
-      if(data[i].localeCompare("&") == 0){
-        data = data.replaceAt(i, "+");
-      }
-      else if (data[i].localeCompare("=") == 0 && i > 2){
-        data = data.replaceAt(i, ":");
-        break;
-      }
-    }
+    console.log(data)
     configure = {
       url: $(this).attr('action'),
       method: 'GET',
@@ -32,29 +24,9 @@ $(document).ready(function() {
     }
     $.ajax(configure)
     .done(function(response){
-      console.log(response['items']);
-      // var configure = {
-      //   url: "/plans/createplan",
-      //   method: 'post',
-      //   data: {repos: response['items']},
-      //   dataType: 'json'
-      // }
-      // $.ajax(configure)
-      // .done(function(response){
-
-      // });
-
-
-      // var source = $('#search-results').html();
-      // var template = Handlebars.compile(source);
-      // var html = template(response["items"]);
-      // debugger
-      // $('#github-search-stuff').html(response['items']);
+      console.log("done");
     });
   });
 });
 
-String.prototype.replaceAt=function(index, character) {
-    return this.substr(0, index) + character + this.substr(index+character.length);
-}
 
