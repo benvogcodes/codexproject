b3bd206d93cdcb1176be3262acfa393960b82731
Michael Ashby <ashby.michael@gmail.com>
2015-09-19 19:16:56 -0700

    reput validations in plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,9 +2,9 @@ class Plan < ActiveRecord::Base
   belongs_to :user
   has_many :repos
 
-  # validates :user_id, presence: true
-  # validates :frequency, presence: true
-  # validates :query, presence: true
+  validates :user_id, presence: true
+  validates :frequency, presence: true
+  validates :query, presence: true
 
   def create_plan(data, user)
     puts '*********************'
