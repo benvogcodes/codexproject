2e0f2592755f71007d4380fe0583487b86ef0058
Michael Ashby <ashby.michael@gmail.com>
2015-09-20 15:00:17 -0700

    get github search results and return cleaned JSON object

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,33 +1,20 @@
 $(document).ready(function() {
-  //When outside, Handlebars was not defined
-  // Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
-  //     lvalue = parseFloat(lvalue);
-  //     rvalue = parseFloat(rvalue);
-
-  //     return {
-  //         '+': lvalue + rvalue,
-  //         '-': lvalue - rvalue,
-  //         '*': lvalue * rvalue,
-  //         '/': lvalue / rvalue,
-  //         '%': lvalue % rvalue
-  //     }[operator];
-  // });
   $('#github-search').on('submit', function(event){
     event.preventDefault();
     var data = $(this).serialize();
     // Make query into OOJS
-    for (var i = 0; i < data.length; i++){
-      if(data[i].localeCompare("&") == 0){
-        data = data.replaceAt(i, "+");
-      }
-      else if (data[i].localeCompare("=") == 0 && i > 2){
-        data = data.replaceAt(i, ":");
-        break;
-      }
-    }
+    // for (var i = 0; i < data.length; i++){
+    //   if(data[i].localeCompare("&") == 0){
+    //     data = data.replaceAt(i, "+");
+    //   }
+    //   else if (data[i].localeCompare("=") == 0 && i > 2){
+    //     data = data.replaceAt(i, ":");
+    //     break;
+    //   }
+    // }
     configure = {
       url: $(this).attr('action'),
-      method: 'GET',
+      method: 'POST',
       data: data
     }
     $.ajax(configure)
