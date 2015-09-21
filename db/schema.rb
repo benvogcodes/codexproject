92a7927718c531052f118428c045b793c835c780
Michael Ashby <ashby.michael@gmail.com>
2015-09-21 09:46:18 -0700

    add name column to plans table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920224409) do
+ActiveRecord::Schema.define(version: 20150921164258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150920224409) do
     t.datetime "updated_at",      null: false
     t.integer  "cards_per_serve"
     t.integer  "serves"
+    t.string   "name"
   end
 
   add_index "plans", ["user_id"], name: "index_plans_on_user_id", using: :btree
