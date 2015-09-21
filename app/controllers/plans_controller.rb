6a96822ee31c038ba39cc8ef16d4ce1331d98ea9
Michael Ashby <ashby.michael@gmail.com>
2015-09-21 15:24:45 -0700

    allow github api calls to filter and sort by stars

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -14,33 +14,33 @@ class PlansController < ApplicationController
 
     @user = current_user
     @data = params
-    name = "#{Time.now.year}/#{Time.now.month}/#{Time.now.day} #{params['language']} #{params['topc']}"
-    new_plan = @user.plans.new(frequency: 1, topic: params['topic'], cards_per_serve: 5, serves: 5, name: name)
-    new_plan.language = params['language']
+    name = "#{Time.now.year}/#{Time.now.month}/#{Time.now.day} #{params['plan']['language']} #{params['plan']['topic']}"
+    new_plan = @user.plans.new(frequency: 1, topic: params['plan']['topic'], cards_per_serve: 5, serves: 5, name: name)
+    new_plan.language = params['plan']['language']
     new_plan.save
 
-    q = "q=#{params['topic']}+language:#{params[:language]}"
-    puts '*************************'
-    puts q
-    puts params[:language]
-    puts new_plan.language
-    puts '*************************'
+    # q = "q=#{params['plan']['topic']}+language:#{params['plan']['language']}"
+    # puts '*************************'
+    # puts q
+    # puts params['language']
+    # puts new_plan.language
+    # puts '*************************'
 
-    @data = Octokit.search_repos(q)
+    # @data = Octokit.search_repos(q)
     # puts '*************************'
     # p @data.items
     # puts '*************************'
 
-    if params['topic'].length > 1
-      topic = params['topic'] + '+'
+    if params['plan']['topic'].length > 1
+      topic = params['plan']['topic'] + '+'
     else
       topic = ''
     end
-    q = "q=#{topic}language:#{params['language']} stars:>5"
+    q = "#{topic}language:#{params['plan']['language']} stars:>100"
     puts '***************************************************'
     puts q
     puts '***************************************************'
-    @data = Octokit.search_repos(q, per_page: 100)
+    @data = Octokit.search_repos(q, {sort: 'stars', order: 'desc', per_page: 100})
     @data = new_plan.create_plan(@data.items, @user)
 
     # puts '*************************'
