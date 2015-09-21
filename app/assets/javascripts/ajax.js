46e0904e7903ded2df7de99a388c70fa3578bee8
benvogcodes <benjamin.voglewede@gmail.com>
2015-09-20 19:39:14 -0700

    Moved fonts directory, plans show page now displays all child repo objects

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-  $('#github-search').on('submit', function(event){
-    event.preventDefault();
-    var data = $(this).serialize();
-    configure = {
-      url: $(this).attr('action'),
-      method: 'POST',
-      data: data
-    }
-    $.ajax(configure)
-    .done(function(response){
-      console.log(response);
-    });
-  });
-});
-var raw_data
+// $(document).ready(function() {
+//   $('#github-search').on('submit', function(event){
+//     event.preventDefault();
+//     var data = $(this).serialize();
+//     configure = {
+//       url: $(this).attr('action'),
+//       method: 'POST',
+//       data: data
+//     }
+//     $.ajax(configure)
+//     .done(function(response){
+//       console.log(response);
+//     });
+//   });
+// });
+// var raw_data
 
 
 
@@ -45,4 +45,3 @@ var raw_data
 // var html = template(response["items"]);
 // debugger
 // $('#github-search-stuff').html(response['items']);
->>>>>>> 740cc17d149e6961d8c0e8c0a0a20b5e197cbc58
