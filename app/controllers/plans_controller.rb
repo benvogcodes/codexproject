e3447477cca9090af362f27e6c684a0bba7bbfba
benvogcodes <benjamin.voglewede@gmail.com>
2015-09-20 19:39:14 -0700

    Moved fonts directory, plans show page now displays all child repo objects

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -14,7 +14,7 @@ class PlansController < ApplicationController
   # end
 
   def create
-    @user = get_current_user
+    @user = current_user
     @data = params
     puts '*************************'
     puts @user.username
@@ -44,10 +44,11 @@ class PlansController < ApplicationController
     p @data
     puts '*************************'
 
-    render json: @data
+    redirect_to action: "show", id: new_plan.id
   end
 
   def show
+    @plan = Plan.find_by(id: params[:id])
   end
 
   def edit
@@ -59,13 +60,13 @@ class PlansController < ApplicationController
   def destroy
   end
 
-  def createplan
-    @repositories = params[:repos]
-    @repositories.each do |repository|
-      puts "*" * 100
-      # fetching property example
-      p repository[1]['full_name']
-    end
-    redirect_to root_path
-  end
+  # def createplan
+  #   @repositories = params[:repos]
+  #   @repositories.each do |repository|
+  #     puts "*" * 100
+  #     # fetching property example
+  #     p repository[1]['full_name']
+  #   end
+  #   redirect_to root_path
+  # end
 end
