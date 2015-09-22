9ed0360d47a44a0e3c5f0f49de388fff86c3a14c
Michael Ashby <ashby.michael@gmail.com>
2015-09-22 10:21:39 -0700

    add destroy dependencies for db resources

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,6 +1,6 @@
 class Repo < ActiveRecord::Base
   belongs_to :plan
-  has_many   :servings
+  has_many   :servings, dependent: :destroy
 
   validates :plan_id, presence: true
   validates :url, presence: true
