ed9e07988c3d71e06189798d155781241fae1ea2
Michael Ashby <ashby.michael@gmail.com>
2015-09-22 14:01:40 -0700

    move some plan creation functionality to private methods

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -10,14 +10,12 @@ class PlansController < ApplicationController
 
   def create
     @user = current_user
-    @data = params
 
     name = "#{Time.now.year}/#{Time.now.month}/#{Time.now.day} #{params['plan']['language']} #{params['plan']['topic']}"
-    new_plan = @user.plans.new(frequency: 1, topic: params['plan']['topic'],
+    new_plan = @user.plans.create(frequency: 1, topic: params['plan']['topic'],
                                cards_per_serve: 5, serves: 5, name: name,
                                twilio: false, sendgrid: false,
                                language: params['plan']['language'])
-    new_plan.save
 
     if params['plan']['topic'].length > 1
       topic = params['plan']['topic'] + '+'
@@ -25,10 +23,7 @@ class PlansController < ApplicationController
       topic = ''
     end
 
-    q = "#{topic}language:#{params['plan']['language']} stars:>100 pushed:>#{DateTime.now - 18.months}"
-    authenticate_github
-    Octokit.auto_paginate = true
-    @data = Octokit.search_repos(q, {sort: 'stars', order: 'desc'})
+    create_query(topic)
     @data = new_plan.create_plan(@data.items, @user)
 
     redirect_to action: "show", id: new_plan.id
@@ -54,4 +49,11 @@ class PlansController < ApplicationController
     redirect_to plans_path
   end
 
+  private
+    def create_query(topic)
+      q = "#{topic}language:#{params['plan']['language']} stars:>100 pushed:>#{DateTime.now - 18.months}"
+      authenticate_github
+      Octokit.auto_paginate = true
+      @data = Octokit.search_repos(q, {sort: 'stars', order: 'desc'})
+    end
 end
