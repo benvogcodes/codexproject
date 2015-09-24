f3cd27953094075889473428f9ef8846fd6e0ce9
Michael Ashby <ashby.michael@gmail.com>
2015-09-23 17:51:07 -0700

    toggle twilio and sendgrid forms on check

--- public/plans/js/plans.js
+++ public/plans/js/plans.js
@@ -2,4 +2,12 @@ $('#cards_divider').click(function() {
   $('#prev_cards_box').toggle('display');
   $('#prev_cards_show_button').toggle(duration=0);
   $('#prev_cards_hide_button').toggle(duration=0);
+});
+
+$('#plan_twilio').click(function() {
+  $('#twilio_form').toggle('display');
+});
+
+$('#plan_sendgrid').click(function() {
+  $('#sendgrid_form').toggle('display');
 })
