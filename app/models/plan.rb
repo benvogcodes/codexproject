271250943da88cfb57dc34f7ff833c8d167685ad
benvogcodes <benjamin.voglewede@gmail.com>
2016-03-23 11:49:06 -0700

    Extract make_cards_and_repos

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -14,47 +14,9 @@ class Plan < ActiveRecord::Base
   def clean_data(data)
     result = []
     items = data || []
-
     number_of_cards = self.cards_per_serve
     number_of_deliveries = self.serves
-
-    number_of_deliveries.times do |j|
-
-      result = []
-
-      number_of_cards.times do |i|
-
-        if items.length > 0
-          item = items.slice!(rand(0..(items.length - 1)))
-
-          new_card = {'id' => item.id,
-            'name' => item.name,
-            'full_name' => item.full_name,
-            'url' => item.url,
-            'html_url' => item.html_url,
-            'description' => trim_description(item.description),
-            'created' => item.created_at,
-            'updated' => item.updated_at,
-            'pushed' => item.pushed_at,
-            'size' => item.size,
-            'stars' => item.stargazers_count,
-            'watchers' => item.watchers_count,
-            'forks' => item.forks_count,
-            'score' => item.score,
-            'user' => item.owner.login
-          }
-
-          # Create a new repo object tied to this plan based on the input hash created above.
-          new_card = build_card(new_card, self)
-        end
-
-        result << new_card if new_card
-      end
-
-      # Creates serving objects tied to this plan, containing the repo objects created above.
-      build_servings(result, self, j)
-    end
-
+    make_cards_and_repos(result, items, number_of_cards, number_of_deliveries)
     result.sort! { |a,b| a.stars <=> b.stars }
   end
 
@@ -79,44 +41,6 @@ class Plan < ActiveRecord::Base
     card
   end
 
-# Creates new repo objects tied to a particular plan.
-  # def build_cards(items, plan)
-  #   result = []
-  #
-  #   items.each do |item|
-  #     card = plan.repos.new(
-  #       served: false,
-  #       size: item.size,
-  #       desc: item['description'],
-  #       url: item['html_url'],
-  #       name: item['name'],
-  #       user: item['user'],
-  #       created: item['created'],
-  #       updated: item['updated'],
-  #       pushed: item['pushed'],
-  #       watchers: item['watchers']
-  #     )
-  #
-  #     card.stars = item['stars'] || 0
-  #     card.forks = item['forks'] || 0
-  #
-  #     result << card if card.save
-  #   end
-  #
-  #   result.sort! { |a,b| a.stars <=> b.stars }
-  # end
-
-# Creates serving objects tied to a particular plan.
-  def build_servings(repositories, plan, num)
-    result = []
-
-    repositories.each do |repository|
-      new_serving = plan.servings.new(repo_id: repository.id, delivery: num)
-      result << new_serving if new_serving.save
-    end
-
-    result
-  end
 
   def current_cards
     cards = []
@@ -136,9 +60,59 @@ class Plan < ActiveRecord::Base
 
   private
 
+  # Creates serving objects tied to a particular plan.
+  def build_servings(repositories, plan, num)
+    result = []
+
+    repositories.each do |repository|
+      new_serving = plan.servings.new(repo_id: repository.id, delivery: num)
+      result << new_serving if new_serving.save
+    end
+
+    result
+  end
+  
+  # Need to extract another method from this, dual responsibility.
+  def make_cards_and_repos(result, items, number_of_cards, number_of_deliveries)
+    number_of_deliveries.times do |j|
+      result = []
+      number_of_cards.times do |i|
+        if items.length > 0
+          item = items.slice!(rand(0..(items.length - 1)))
+          new_card = parse_item_details(item)
+          # Create a new repo object tied to this plan based on the input hash created above.
+          finalized_card = build_card(new_card, self)
+        end
+        result << finalized_card if finalized_card
+      end
+      # Creates serving objects tied to this plan, containing the repo objects created above.
+      build_servings(result, self, j)
+    end
+  end
+
+  def parse_item_details(item)
+    {'id' => item.id,
+      'name' => item.name,
+      'full_name' => item.full_name,
+      'url' => item.url,
+      'html_url' => item.html_url,
+      'description' => trim_description(item.description),
+      'created' => item.created_at,
+      'updated' => item.updated_at,
+      'pushed' => item.pushed_at,
+      'size' => item.size,
+      'stars' => item.stargazers_count,
+      'watchers' => item.watchers_count,
+      'forks' => item.forks_count,
+      'score' => item.score,
+      'user' => item.owner.login
+    }
+  end
+
   # Truncate repo description if longer than 255 chars.
   def trim_description(description)
     description = description.slice(0, 255) + '...' unless description.length <= 255
     description
   end
+
 end
